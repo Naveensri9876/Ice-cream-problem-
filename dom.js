47f5ed41812e40663a563b454d48c2cbaa47584d
Naveensri9876 <142909201+Naveensri9876@users.noreply.github.com>
2024-10-04 14:16:49 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,8 @@
+const flavour={1:"Apple",2:"mango",3:"Orange"};
+const quantity={1:"2",2:"3",3:"2"};
+const cost={1:150,2:60,3:70};
+flavour[4]="vennila";
+flavour[2]="strawberry";
+console.log(flavour)
+const count=Object.keys(flavour).length;
+console.log(count)
